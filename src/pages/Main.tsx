a8a826b180c772f6bb4c6e702179764d7c7671c3
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-23 17:07:26 +0300

    some setups

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../component/Header";
+import { Container } from "@mui/material";
+
+const Main = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Main;
