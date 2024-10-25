4559218f088972a9d6dc3fcef9bc6706aba33ec0
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-25 17:55:51 +0300

    handleChange,handleSubmit

--- src/component/AddTodo.tsx
+++ src/component/AddTodo.tsx
@@ -1,16 +1,43 @@
-import { Save } from '@mui/icons-material'
-import { Box, Button, TextField } from '@mui/material'
+import { Save } from "@mui/icons-material";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
 
+const AddTodo = ({addTodo}) => {
+  // const [task, setTask] = useState("") //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde initial değerine göre otomatik type ataması yapıyor.
+  const [task, setTask] = useState("");
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setTask(e.target.value)
 
-const AddTodo = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(task)
+  }
   return (
-    <Box component= "form" sx={{display:{xs:"block", sm:"flex"}, justifyContent: {xs:1, sm:"auto"}, height:{xs:"120px", sm:"80px"}}}>
-    <TextField variant='outlined' color='success' sx={{minWidth: {xs:"100%", sm: "50%"}, height:"50px", m:1}} />
-    <Button variant='contained' endIcon= {<Save/>} color='success' sx={{minWidth: {xs:"100%", sm:"10%"}, height:"55px", m:1}} >
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: { xs: "block", sm: "flex" },
+        justifyContent: { xs: 1, sm: "auto" },
+        height: { xs: "120px", sm: "80px" },
+      }}
+    >
+      <TextField
+        variant="outlined"
+        color="success"
+        sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+        // onChange={e =>setTask(e.target.value) }
+        onChange = {handleChange}
+      />
+      <Button
+        variant="contained"
+        endIcon={<Save />}
+        color="success"
+        sx={{ minWidth: { xs: "100%", sm: "10%" }, height: "55px", m: 1 }}
+      >
         Save Todo
-    </Button>
-    </Box >
-  )
-}
+      </Button>
+    </Box>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../component/Header";
 import { Container } from "@mui/material";
 import AddTodo from "../component/AddTodo";
+import axios from "axios";
+
+interface ITodoType {
+  task: string;
+  isDone: boolean;
+  id: string | number; // id string veya number olabilir.
+  owner?: string; //! bu alan zorunlu değil. bu veri gelmişse type da string
+}
+
+const url = "https://634ac3fc5df952851418480f.mockapi.io/api/todos";
 
 const Main = () => {
+  // const [todos, setTodos] = useState([] as ITodoType[]);
+  // const [todos, setTodos] = useState<Array<ITodoType>>([]);
+  const [todos, setTodos] = useState<ITodoType[]>([]); //yaygın kullanım
+  console.log(todos);
+  const getTodos = async () => {
+    try {
+      const { data } = await axios(url);
+      // const {data} = await axios<ITodoType[]>(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //1.yol
+  // const addTodo = async (task:string) => {
+  //   try {
+  //     await axios.post(url, { task, isDone: false });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  //2.yol
+  type AddFn = (task:string) => Promise<void>
+  const addTodo:AddFn = async (task:string) => {
+    try {
+      await axios.post(url, { task, isDone: false });
+      getTodos()
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <Container>
       <Header />
-      <AddTodo/>
+      <AddTodo addTodo={addTodo} />
     </Container>
   );
 };
