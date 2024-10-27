8d4e73ed220b3628872b67ae4e6d7f0ad7cc2903
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-27 10:59:00 +0300

    TodoItem.tsx

---
+++ src/component/TodoItem.tsx
@@ -0,0 +1,25 @@
+import { DeleteOutline } from '@mui/icons-material'
+import { IconButton, ListItem, ListItemText } from '@mui/material'
+import React, { FC } from 'react'
+
+interface ITodoItem {
+    todo:ITodoType
+}
+const TodoItem: FC<ITodoItem> = ({todo}) => {
+    return (
+      <ListItem
+      disableGutters
+      sx={{padding:"1rem", cursor:"pointer"}}
+      secondaryAction={
+        <IconButton aria-label="comment">
+          <DeleteOutline sx={{"&:hover":{color:"red"}}} />
+        </IconButton>
+      }
+    >
+      <ListItemText primary={todo.task} />
+    </ListItem>
+    )
+  }
+  
+
+export default TodoItem
